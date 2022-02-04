08fd4af9528a2b1ce8038ed0bebb0456c446a6ab
Natan Rincon Luna <rincon.luna.natan@gmail.com>
2022-02-04 15:37:58 -0800

    Final update w/o bonus

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-(function() {
-  'use strict';
 
-  // YOUR CODE HERE
-})();
+
+//global variables 
+const stopBotton = document.getElementById('stopButton');
+const stopLight = document.getElementById("stopLight");
+
+const slowButton = document.getElementById("slowButton");
+const slowLight = document.getElementById("slowLight");
+
+const goButton = document.getElementById("goButton");
+const goLight = document.getElementById("goLight");
+
+//click on and off
+stopBotton.addEventListener('click', function(){
+  stopLight.classList.toggle("stop")
+
+});
+
+slowButton.addEventListener('click', function(){
+  slowLight.classList.toggle("slow")
+});
+
+goButton.addEventListener('click', function (){
+  goLight.classList.toggle("go")
+});
+
+// console.log
+stopBotton.addEventListener('mouseenter', function(){
+console.log("Mouse Entered stop button")
+});
+
+stopBotton.addEventListener('mouseleave', function(){
+  console.log("Mouse Left stop button")
+});
+
+slowButton.addEventListener('mouseenter', function(){
+  console.log("Mouse Entered slow button")
+  });
+slowButton.addEventListener('mouseleave', function(){
+    console.log("Mouse Left slow button")
+});
+
+goButton.addEventListener('mouseenter', function(){
+  console.log("Mouse Entered go button")
+  });
+goButton.addEventListener('mouseleave', function(){
+    console.log("Mouse Left go button")
+});
+const bulb = document.getElementByClassName("bulb stop")
+console.log(bulb)
